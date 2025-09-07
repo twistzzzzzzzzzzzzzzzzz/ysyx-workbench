383cf7fe74ef38517563b120a7fd8308dcd04904
twistzzzzzzzzzzzzzzzzz <3329208943@qq.com>
2025-09-07 23:35:37 +0800

    1

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -98,7 +98,6 @@ static void exec_once(Decode *s, vaddr_t pc) {
       MUXDEF(CONFIG_ISA_x86, s->snpc, s->pc), (uint8_t *)&s->isa.inst, ilen);
 #endif
 }
-}
 
 static void execute(uint64_t n) {
   Decode s;
---
+++ nemu/src/monitor/sdb/expr.h
@@ -0,0 +1,10 @@
+// expr.h
+#ifndef EXPR_H
+#define EXPR_H
+
+#include <stdbool.h> // 如果用到 bool 类型
+
+// 声明 expr() 函数
+int expr(const char *expression, bool *success);
+
+#endif
