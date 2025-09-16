377163077bb7a7e28c7ff0f37b871a1999223d10
twistzzzzzzzzzzzzzzzzz <3329208943@qq.com>
2025-09-16 23:53:27 +0800

    > run NEMU ysyx_22040000 张硕 Linux ubuntu 6.8.0-79-generic #79~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 15 16:54:53 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  08:27:35 up 34 min,  1 user,  load average: 0.21, 0.31, 0.35

--- nemu/src/utils/disasm.c
+++ nemu/src/utils/disasm.c
@@ -60,6 +60,7 @@ void init_disasm() {
 }
 
 void disassemble(char *str, int size, uint64_t pc, uint8_t *code, int nbyte) {
+  printf("Disassemble: pc=0x%lx (pc%%4=%lu), nbyte=%d, code=0x%x\n", pc, pc%4, nbyte, *(uint32_t*)code);
 	cs_insn *insn;
 	size_t count = cs_disasm_dl(handle, code, nbyte, pc, 0, &insn);
   assert(count == 1);
